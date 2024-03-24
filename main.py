f7504feaff366986e8f11d0ae29be6711d88ed78
Chuck MacKinnon <chuckwm@gmail.com>
2024-03-24 13:02:31 -0400

    Adding my version, can compare to theirs

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 def main():
-    with open("books/frankenstein.txt") as f: 
-    # ... 
-        file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+    book = "books/frankenstein.txt"
+    text = get_book_text(book)
+    num_words = count_words(text)
+    counted_letters = count_letters(text)
+    best = sorted([(v,k) for (k,v) in counted_letters.items()], reverse=True)
+    report = create_report(book, best)
+    print(report)
+
+def get_book_text(book):
+    with open(book) as f:
+        return f.read()
+
+def count_words(text):
+    count = text.split()
+    return len(count)
+
+def count_letters(text):
+    letters = dict()
+    for letter in text.lower():
+        if letter in 'abcdefghijklmnopqrstuvwxyz':
+            letters[letter] = letters.get(letter,0)+1
+    return letters
+
+
+def create_report(book, best):
+    body = ""
+    body += f"--- Begin report of {book} ---\n"
+    for count, letter in best:
+        body += f"The '{letter}' character was found {count} times\n"
+    body += "--- End report ---"
+    report = f"""
+{body}
+    """
+    return report
+#    letters.add(
 
 if __name__ == '__main__':
     main()
+
 
 
 
